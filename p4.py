390e0999acd7d2e099fb7322bffaf75a896d55e9
Shana Friedman <friedman.shana@gmail.com>
2015-02-16 22:25:10 -0500

    problem 4 correct

--- p4.py
+++ p4.py
@@ -1,7 +1,16 @@
-import sys
+def ispalindrome(n):
+	length = len(n);
+	for i in range(0, length/2):
+		if n[i] != n[length-i-1]:
+			return False
+	return True
 
-def main():
-  print 'hi'
+largest_palindrome = 0
 
-if __name__ == '__main__':
-  main()
+for i in range(100, 999):
+	for j in range(i, 999):
+		prod = i * j
+		if ispalindrome(str(prod)) and prod > largest_palindrome:
+			largest_palindrome = prod
+
+print largest_palindrome
