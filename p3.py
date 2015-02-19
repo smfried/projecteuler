d79d30ef42f5a29ea3601a054b9599f80e5c137e
Shana Friedman <friedman.shana@gmail.com>
2015-02-19 14:11:45 -0500

    Solved p3 - largest prime factor

--- p3.py
+++ p3.py
@@ -1,17 +1,20 @@
+import math
+
 large_num = 600851475143
 
 def isprime(num):
-	for i in range(2, num-1):
+	s = math.sqrt(num)
+	c = math.ceil(s)
+	sqrt = int(s)
+	for i in range(2, sqrt+1):
 		if(num % i == 0):
 			return False
 	return True
 
-print isprime(large_num)
+sqrt = math.sqrt(large_num)
+ceil = math.ceil(sqrt)
+int_sqrt = int(sqrt)
 
-# def getfactor():
-# 	for i in range(large_num-1, 3, -1):
-# 		print i
-# 		if large_num % i == 0 and isprime(i):
-# 			return i
-
-# print getfactor()
+for i in range(int_sqrt, 3, -1):
+	if large_num % i == 0 and isprime(i):
+		print i
